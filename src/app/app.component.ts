4fbdfada738f76d5639bc5457b43056236896ed4
Mike Huang <wellwind@mail2000.com.tw>
2020-02-23 10:42:03 +0800

    練習元件資料綁訂到畫面上

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularbasic20200223';
+  title = 'Angular';
+  subtitle = 'A place to share your <u>knowledge</u>.';
 }
